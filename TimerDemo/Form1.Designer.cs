5b7bae1d247e98ae8aec3005a57462d0fa940a2a
Phil <phil.cooling@gmail.com>
2018-09-03 22:06:11 +0100

    Add Timer code

--- TimerDemo/Form1.Designer.cs
+++ TimerDemo/Form1.Designer.cs
@@ -32,7 +32,7 @@ namespace TimerDemo
         {
             this.components = new System.ComponentModel.Container();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.label1 = new System.Windows.Forms.Label();
+            this.timerDisplay = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // notifyIcon1
@@ -42,26 +42,26 @@ namespace TimerDemo
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
-            // label1
+            // timerDisplay
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 3);
-            this.label1.Margin = new System.Windows.Forms.Padding(3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 39);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "00:00:00";
-            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
-            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
-            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            this.timerDisplay.AutoSize = true;
+            this.timerDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerDisplay.Location = new System.Drawing.Point(3, 3);
+            this.timerDisplay.Margin = new System.Windows.Forms.Padding(3);
+            this.timerDisplay.Name = "timerDisplay";
+            this.timerDisplay.Size = new System.Drawing.Size(151, 39);
+            this.timerDisplay.TabIndex = 1;
+            this.timerDisplay.Text = "00:00:00";
+            this.timerDisplay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.timerDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.timerDisplay.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
             // 
             // Form1
             // 
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(158, 46);
             this.ControlBox = false;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timerDisplay);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -80,7 +80,7 @@ namespace TimerDemo
         #endregion
 
         private NotifyIcon notifyIcon1;
-        private Label label1;
+        private Label timerDisplay;
     }
 }
 
